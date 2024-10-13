ce6a86b5cf0f095eb27c4fcddf43fb47f133623e
brevin33 <brevin.bell0@gmail.com>
2024-10-13 15:32:26 -0500

    fixed lsp c++ lsp, and deubgger and added more plugins

--- lua/b/set.lua
+++ lua/b/set.lua
@@ -26,62 +26,244 @@ vim.o.scrolloff = 9999
 vim.o.signcolumn = "yes"
 vim.o.updatetime = 50
 vim.o.hls = true
+vim.o.ruler = false
+vim.o.spr = true
+vim.o.aw = true
+vim.o.awa = true
+
+vim.o.laststatus = 0
+vim.opt.fillchars = { eob = " " }
+vim.o.sc = false
+vim.o.stal = 0
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.o.autoindent = true
+vim.o.expandtab = false
 
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 vim.api.nvim_create_autocmd('CursorMoved', {
-    group = vim.api.nvim_create_augroup('auto-hlsearch', { clear = true }),
-    callback = function()
-        if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
-            vim.schedule(function() vim.cmd.nohlsearch() end)
-        end
-    end
+	group = vim.api.nvim_create_augroup('auto-hlsearch', { clear = true }),
+	callback = function()
+		if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+			vim.schedule(function() vim.cmd.nohlsearch() end)
+		end
+	end
 })
 
-require('lualine').setup {
-    options = {
-        icons_enabled = true,
-        theme = 'auto',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-        }
-    },
-    sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = {},
-        lualine_z = {}
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
+vim.api.nvim_create_autocmd({ "BufLeave", "InsertLeave" }, {
+	group = vim.api.nvim_create_augroup('auto-save', { clear = true }),
+	callback = function()
+		pcall(vim.cmd, "wa")
+	end
+})
+
+require("toggleterm").setup {
+	size = 20,
+	hide_numbers = true, -- hide the number column in toggleterm buffers
+	autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+	close_on_exit = true, -- close the terminal window when the process exits
+	clear_env = false, -- use only environmental variables from `env`, passed to jobstart()
+	auto_scroll = true, -- automatically scroll to the bottom on terminal output
+	start_in_insert = false,
+	direction = "float",
+	shell = "PowerShell",
+	-- This field is only relevant if direction is set to 'float'
+	float_opts = {
+		border = 'single',
+		width = 140,
+		height = 40,
+		zindex = 99,
+		title_pos = 'center',
+	},
 }
+
+require("oil").setup({
+	-- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+	-- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
+	default_file_explorer = true,
+	-- Id is automatically added at the beginning, and name at the end
+	-- See :help oil-columns
+	columns = {
+		"icon",
+		-- "permissions",
+		-- "size",
+		-- "mtime",
+	},
+	-- Buffer-local options to use for oil buffers
+	buf_options = {
+		buflisted = false,
+		bufhidden = "hide",
+	},
+	-- Window-local options to use for oil buffers
+	win_options = {
+		wrap = false,
+		signcolumn = "no",
+		cursorcolumn = false,
+		foldcolumn = "0",
+		spell = false,
+		list = false,
+		conceallevel = 3,
+		concealcursor = "nvic",
+	},
+	-- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+	delete_to_trash = false,
+	-- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
+	skip_confirm_for_simple_edits = false,
+	-- Selecting a new/moved/renamed file or directory will prompt you to save changes first
+	-- (:help prompt_save_on_select_new_entry)
+	prompt_save_on_select_new_entry = true,
+	-- Oil will automatically delete hidden buffers after this delay
+	-- You can set the delay to false to disable cleanup entirely
+	-- Note that the cleanup process only starts when none of the oil buffers are currently displayed
+	cleanup_delay_ms = 2000,
+	lsp_file_methods = {
+		-- Enable or disable LSP file operations
+		enabled = true,
+		-- Time to wait for LSP file operations to complete before skipping
+		timeout_ms = 1000,
+		-- Set to true to autosave buffers that are updated with LSP willRenameFiles
+		-- Set to "unmodified" to only save unmodified buffers
+		autosave_changes = false,
+	},
+	-- Constrain the cursor to the editable parts of the oil buffer
+	-- Set to `false` to disable, or "name" to keep it on the file names
+	constrain_cursor = "editable",
+	-- Set to true to watch the filesystem for changes and reload oil
+	watch_for_changes = false,
+	-- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+	-- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+	-- Additionally, if it is a string that matches "actions.<name>",
+	-- it will use the mapping at require("oil.actions").<name>
+	-- Set to `false` to remove a keymap
+	-- See :help oil-actions for a list of all available actions
+	keymaps = {
+		["g?"] = "actions.show_help",
+		["<CR>"] = "actions.select",
+		["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+		["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+		["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+		["<C-p>"] = "actions.preview",
+		["<C-c>"] = "actions.close",
+		["<C-l>"] = "actions.refresh",
+		["-"] = "actions.parent",
+		["_"] = "actions.open_cwd",
+		["`"] = "actions.cd",
+		["!"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory", mode = "n" },
+		["<leader>s"] = "actions.change_sort",
+		["<leader>x"] = "actions.open_external",
+		["g."] = "actions.toggle_hidden",
+		["g\\"] = "actions.toggle_trash",
+	},
+	-- Set to false to disable all of the above keymaps
+	use_default_keymaps = false,
+	view_options = {
+		-- Show files and directories that start with "."
+		show_hidden = false,
+		-- This function defines what is considered a "hidden" file
+		is_hidden_file = function(name, bufnr)
+			return vim.startswith(name, ".")
+		end,
+		-- This function defines what will never be shown, even when `show_hidden` is set
+		is_always_hidden = function(name, bufnr)
+			return false
+		end,
+		-- Sort file names in a more intuitive order for humans. Is less performant,
+		-- so you may want to set to false if you work with large directories.
+		natural_order = true,
+		-- Sort file and directory names case insensitive
+		case_insensitive = false,
+		sort = {
+			-- sort order can be "asc" or "desc"
+			-- see :help oil-columns to see which columns are sortable
+			{ "type", "asc" },
+			{ "name", "asc" },
+		},
+	},
+	-- Extra arguments to pass to SCP when moving/copying files over SSH
+	extra_scp_args = {},
+	-- EXPERIMENTAL support for performing file operations with git
+	git = {
+		-- Return true to automatically git add/mv/rm files
+		add = function(path)
+			return false
+		end,
+		mv = function(src_path, dest_path)
+			return false
+		end,
+		rm = function(path)
+			return false
+		end,
+	},
+	-- Configuration for the floating window in oil.open_float
+	float = {
+		-- Padding around the floating window
+		padding = 2,
+		max_width = 0,
+		max_height = 0,
+		border = "rounded",
+		win_options = {
+			winblend = 0,
+		},
+		-- preview_split: Split direction: "auto", "left", "right", "above", "below".
+		preview_split = "auto",
+		-- This is the config that will be passed to nvim_open_win.
+		-- Change values here to customize the layout
+		override = function(conf)
+			return conf
+		end,
+	},
+	-- Configuration for the actions floating preview window
+	preview = {
+		-- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+		-- min_width and max_width can be a single value or a list of mixed integer/float types.
+		-- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
+		max_width = 0.9,
+		-- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
+		min_width = { 40, 0.4 },
+		-- optionally define an integer/float for the exact width of the preview window
+		width = nil,
+		-- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+		-- min_height and max_height can be a single value or a list of mixed integer/float types.
+		-- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
+		max_height = 0.9,
+		-- min_height = {5, 0.1} means "the greater of 5 columns or 10% of total"
+		min_height = { 5, 0.1 },
+		-- optionally define an integer/float for the exact height of the preview window
+		height = nil,
+		border = "rounded",
+		win_options = {
+			winblend = 0,
+		},
+		-- Whether the preview window is automatically updated when the cursor is moved
+		update_on_cursor_moved = true,
+	},
+	-- Configuration for the floating progress window
+	progress = {
+		max_width = 0.9,
+		min_width = { 40, 0.4 },
+		width = nil,
+		max_height = { 10, 0.9 },
+		min_height = { 5, 0.1 },
+		height = nil,
+		border = "rounded",
+		minimized_border = "none",
+		win_options = {
+			winblend = 0,
+		},
+	},
+	-- Configuration for the floating SSH window
+	ssh = {
+		border = "rounded",
+	},
+	-- Configuration for the floating keymaps help window
+	keymaps_help = {
+		border = "rounded",
+	},
+})
